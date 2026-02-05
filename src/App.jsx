28612f851228f70af00663c775824c31fb6a7778
Felipe Bezerra Joazeiro <felipejoazeiro@gmail.com>
2026-02-05 16:48:02 -0300

    Criação de Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,45 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  
+// no react, componentes são funções que retornam trechos de interface (HTML)
 
+
+// props -> propriedades que são passadas para o componente
+function TitleForm(props){
+  return (
+    <h2>{props.children}</h2>
+  );
+}
+
+function FormFieldset({children}){
+  return (
+    {children}
+  );
+}
+
+function Label({children, htmlFor}){
+  return (<label htmlFor={htmlFor}>{children}</label>);
+}
+
+function Input(props){
+  return (<input {...props} />);
+}
+
+function EventForm(){
+  return (
+    <form className='form-event'>
+      <TitleForm> Cadastro de evento </TitleForm>
+      <FormFieldset> 
+        <fieldset>
+          <Label htmlFor="name"> Nome do evento </Label>
+          <Input type="text" id='name' placeholder='Summer dev hits'/>
+        </fieldset>
+      </FormFieldset>
+    </form>
+  );
+}
+
+function App() {
   return (
    <main>
     <header>
@@ -14,6 +50,7 @@ function App() {
     <section>
       <img src="/banner.png" alt="" />
     </section>
+    <EventForm />
    </main>
   )
 }
