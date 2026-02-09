8a0595838f17812df0f67baca711761e7c1c228d
Felipe Bezerra Joazeiro <felipejoazeiro@gmail.com>
2026-02-09 09:39:06 -0300

    Preparando o layout dos  cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { EventForm } from './components/EventForm'
+import { Theme } from './components/Theme'
 
+
+const exm = [
+  {
+    id: 1,
+    name: "Front-end",
+  },
+  {
+    id: 2,
+    name: "Back-end",
+  },
+  {
+    id: 3,
+    name: "DevOps",
+  },
+  {
+    id: 4,
+    name: "IA",
+  },
+  {
+    id: 5,
+    name: "Data Science",
+  },
+  {
+    id: 6,
+    name: "Cloud",
+  }
+];
 
 function App() {
   return (
@@ -15,6 +43,24 @@ function App() {
       <img src="/banner.png" alt="" />
     </section>
     <EventForm />
+    <section>
+      <Theme theme={exm[0]} />
+    </section>
+    <section>
+      <Theme theme={exm[1]} />
+    </section>
+    <section>
+      <Theme theme={exm[2]} />
+    </section>
+    <section>
+      <Theme theme={exm[3]} />
+    </section>
+    <section>
+      <Theme theme={exm[4]} />
+    </section>
+    <section>
+      <Theme theme={exm[5]} />
+    </section>
    </main>
   )
 }
---
+++ src/components/Theme/index.jsx
@@ -0,0 +1,7 @@
+import './theme-styles.css'
+
+export function Theme({ theme }) {
+    return (
+        <h3>{theme.name}</h3>
+    )
+}
