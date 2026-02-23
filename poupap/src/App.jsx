90f8869aa51df9b5e8d356eb4a915fbf82a592fd
Felipe Bezerra Joazeiro <felipejoazeiro@gmail.com>
2026-02-23 16:45:27 -0300

    Estilização fo Aside

--- poupap/src/App.jsx
+++ poupap/src/App.jsx
@@ -1,13 +1,10 @@
-import logo from './assets/logo.svg'
 import { Container } from './components/Container'
 import { Aside } from './components/Aside'
 
 function App() {
   return (
     <Container>
-      <Aside>
-        <img src={logo} alt="Logo" />
-      </Aside>
+      <Aside/>
       <h1>
         Poupapp - O aplicativo de controle financeiro que vai transformar a sua vida!
       </h1>
--- poupap/src/components/Aside/index.jsx
+++ poupap/src/components/Aside/index.jsx
@@ -1,5 +1,11 @@
 import styles from './aside.module.css';
+import logo from '../../assets/logo.svg';
 
 export const Aside = ({ children }) => {
-  return <aside className={styles.aside}>{children}</aside>;
+  return <aside className={styles.aside}>
+    <img src={logo} alt="Logo" />
+    <footer className={styles.footer}>
+        Desenvolvido sem fins lucrativos por Alura e Tecboard.
+    </footer>
+  </aside>;
 };
