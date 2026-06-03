e68c6ab03f0c77a42e9fba155f0179147c4ed7e6
Felipe Bezerra Joazeiro <felipejoazeiro@gmail.com>
2026-06-03 16:39:23 -0300

    Correção

--- Node.js Career/Course 6/server/app.js
+++ Node.js Career/Course 6/server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const books = require("./books");
+const books = require("./routes/book");
+const favorites = require("./routes/favorites");
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,7 @@ app.use(express.json());
 const port = 8000;
 
 app.use("/books", books);
+app.use("/favorites", favorites);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
